a1874ca143d0c3da262b0781c77d5a86103a70d6
upitroma <55750919+upitroma@users.noreply.github.com>
2022-02-07 13:36:49 -0500

    add button to connect to metamask

---
+++ src/connectButton.js
@@ -0,0 +1,23 @@
+// ConnectButton.tsx
+import { Button, Box, Text } from "@chakra-ui/react";
+import { useEthers, useEtherBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
+
+export default function ConnectButton() {
+  const {activateBrowserWallet, account } = useEthers();
+  const etherBalance = useEtherBalance(account);
+
+  function handleConnectWallet() {
+    activateBrowserWallet();
+  }
+
+  return account ? (
+    <Box>
+      <div>
+      {etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3)} ETH
+      </div>
+    </Box>
+  ) : (
+    <Button onClick={handleConnectWallet}>Connect to a wallet</Button>
+  );
+}
