85e59396e08a45c4c30bac01c2a8b55d018154fe
upitroma <55750919+upitroma@users.noreply.github.com>
2022-03-23 13:12:08 -0400

    home page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import './App.css';
 import ConnectButton from "./connectButton";
 
 import scroll from "./images/scroll.png" 
+import background from "./images/bg.jpg" 
 
 function App() {
   return (
-    <div className="App">
-      
-      <header className="App-header">
-        <div className="title">
+    <div className="App" >
+      <img src={background} classname="bg" height="2000vmin" max-width="100%" object-fit="contain"/>
+      <header className="App-header" >
+        
+        <div className="title" >
           <img src={scroll} class="img" alt="title"/>
           <div class="centered">Noble Fantasy Tournament</div>
         </div> 
-        <div className="connectButton">
-          {/* <img src={scroll} class="img" alt="title"/> */}
-          {/* <div class="centered">Connect to metamask</div> */}
-          <ConnectButton/>
+
+        <ConnectButton/>
+
+        <div className="playButton">
+          <img src={scroll} class="img" alt="title"/>
+          <div class="centered">Play</div>
         </div>
-        
-      </header>      
+      </header>  
+
+      <body>
+        testing text
+      </body>    
     </div>
   );
 }
