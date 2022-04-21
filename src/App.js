5b494d35e98fb0ad8bc013ca3ddf5c5c410502e0
ZapDark <jackaru@me.com>
2022-04-21 09:27:03 -0400

    Token

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ConnectButton from "./connectButton";
 
 import scroll from "./images/scroll.png" 
 import background from "./images/bg.jpg" 
+import { useEffect } from 'react';
+import { TextField } from '@material-ui/core';
+import { Button } from '@chakra-ui/react';
 
 const { abi } = require('./artifacts/contracts/NobleToken.json');
 
@@ -18,26 +21,69 @@ function App() {
       const web3Modal = new Web3Modal({
 
         cacheProvider: true
+      });
 
-    });
+      const connection = await web3Modal.connect();
 
-    const connection = await web3Modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
 
-    const provider = new ethers.providers.Web3Provider(connection);
+      const {chainId} = await provider.getNetwork();
 
-    const {chainId} = await provider.getNetwork();
+      const signer = provider.getSigner();
 
-    const signer = provider.getSigner();
+      const contract = new Contract('0xDee20e4F4d533Af08E1a93E772284b15b9f19beA', abi, signer);
 
-    const contract = new Contract('0x96c2c1781E6cE352b3d5c2c2a6bF7f107DaeDF78', abi, signer);
-
-    resolve({contract});
-
+      resolve({contract});
     })
   }
+
+  const mintToken = async (tokenName) => {
+    if (!tokenName) return;
+
+    contract.mintToken(tokenName).then((tx) => {
+
+      tx.wait().then(() => {
+
+        setLogMessage('Token minted.');
+
+      });
+
+    }).catch((err) => setLogMessage(err.message));
+
+  };
+
+  const getAllTokens = async () => {
+    const tokens = await contract.getAllTokens();
+    setTokens(tokens);
+  };
+
+  const getMyTokens = async () => {
+    const myTokens = await contract.getMyTokens();
+    setMyTokens(myTokens);
+  }
+
+  useEffect(() => {
+
+    initWeb3().then(async ({contract}) => {
+
+      setContract(contract);
+
+      const tokens = await contract.getAllTokens();
+
+      setTokens(tokens);
+
+    }).catch((err) => {
+
+      console.log(err);
+
+      setLogMessage(err);
+    });
+
+  }, []);
+
   return (
     <div className="App" >
-      <img src={background} classname="bg" height="2000vmin" max-width="100%" object-fit="contain"/>
+      <img src={background} className="bg" height="2000vmin" max-width="100%" object-fit="contain"/>
       <header className="App-header" >
         
         <div className="title" >
@@ -55,6 +101,15 @@ function App() {
 
       <body>
         testing text
+        <form>
+          <h2> Mint New Token </h2>
+          <Button onClick={() => mintToken()}> Mint Token </Button>
+        </form>
+        <br />
+        <div>
+          <Button onClick={getMyTokens}>My Minted Tokens</Button>
+          <Button onClick={getAllTokens}>All Minted Tokens</Button>
+        </div>
       </body>    
     </div>
   );
